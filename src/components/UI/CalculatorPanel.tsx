982fda6be0f1735329f968111873adedebfbc7af
neda97e <76654768+neda97e@users.noreply.github.com>
2022-09-20 20:22:21 +0430

    fix: update calculator panel
    update calculator panel to generate buttons in a loop

--- src/components/UI/CalculatorPanel.tsx
+++ src/components/UI/CalculatorPanel.tsx
@@ -1,32 +1,40 @@
 import Button from './Button';
+import Input from './Input';
+import OPERATOR from '../../models/Operator';
+import ButtonSt from '../../models/ButtonSt';
+import BUTTONTYPE from '../../models/ButtonType';
+
+const calcButtons = [
+  new ButtonSt('op1', OPERATOR.SUM, BUTTONTYPE.OPERATOR),
+  new ButtonSt('op2', OPERATOR.SUB, BUTTONTYPE.OPERATOR),
+  new ButtonSt('op3', OPERATOR.MULT, BUTTONTYPE.OPERATOR),
+  new ButtonSt('op4', OPERATOR.DIV, BUTTONTYPE.OPERATOR),
+  new ButtonSt('num7', '7', BUTTONTYPE.OPERAND),
+  new ButtonSt('num8', '8', BUTTONTYPE.OPERAND),
+  new ButtonSt('num9', '9', BUTTONTYPE.OPERAND),
+  new ButtonSt('op5', OPERATOR.EQU, BUTTONTYPE.OPERATOR),
+  new ButtonSt('num4', '4', BUTTONTYPE.OPERAND),
+  new ButtonSt('num5', '5', BUTTONTYPE.OPERAND),
+  new ButtonSt('num6', '6', BUTTONTYPE.OPERAND),
+  new ButtonSt('num1', '1', BUTTONTYPE.OPERAND),
+  new ButtonSt('num2', '2', BUTTONTYPE.OPERAND),
+  new ButtonSt('num3', '3', BUTTONTYPE.OPERAND),
+  new ButtonSt('num0', '0', BUTTONTYPE.OPERAND),
+  new ButtonSt('dot', '.', BUTTONTYPE.OPERAND),
+  new ButtonSt('op6', OPERATOR.CLC, BUTTONTYPE.OPERATOR)
+];
 
 const CalculatorPanel = () => {
   const equallHandler = (event: React.FormEvent) => {
     event.preventDefault();
-    return;
   };
-
   return (
     <div className='grid h-screen place-items-center'>
-      <div className='border-gray-500 p-3 w-[400px] grid grid-cols-4 gap-x-3 gap-y-4'>
-        <div className='border min-h-[60px] col-span-4'></div>
-        <div className='bg-gray-400 min-h-[50px]'></div>
-        <div className='bg-gray-700 min-h-[50px]'></div>
-        <div className='bg-gray-700 min-h-[50px]'></div>
-        <div className='bg-gray-700 min-h-[50px]'></div>
-        <div className='bg-gray-700 min-h-[50px]'></div>
-        <div className='bg-gray-700 min-h-[50px]'></div>
-        <div className='bg-gray-700 min-h-[50px]'></div>
-        <div className='bg-gray-700 min-h-[50px] row-span-4'></div>
-        <div className='bg-gray-700 min-h-[50px]'></div>
-        <div className='bg-gray-700 min-h-[50px]'></div>
-        <div className='bg-gray-700 min-h-[50px]'></div>
-        <div className='bg-gray-700 min-h-[50px]'></div>
-        <div className='bg-gray-700 min-h-[50px]'></div>
-        <div className='bg-gray-700 min-h-[50px]'></div>
-        <div className='bg-gray-700 min-h-[50px]'></div>
-        <div className='bg-gray-700 min-h-[50px]'></div>
-        <div className='bg-gray-700 min-h-[50px]'></div>
+      <div className='border-2 border-neutral-200 shadow-md p-4 w-[400px] grid grid-cols-4 gap-x-3 gap-y-4'>
+        <Input classes='border-2 border-neutral-200 h-[60px] col-span-4' />
+        {calcButtons.map((btn) => {
+          return <Button btn={btn} onClick={equallHandler} />;
+        })}
       </div>
     </div>
   );

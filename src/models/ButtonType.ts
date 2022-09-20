92c066f64266efe48e789b30b9e57dfbab8a16cd
neda97e <76654768+neda97e@users.noreply.github.com>
2022-09-20 20:17:02 +0430

    feat: add Operator and ButtonType
    add operator and buttontype enum for calculator

---
+++ src/models/ButtonType.ts
@@ -0,0 +1,6 @@
+enum BUTTONTYPE {
+  OPERATOR = 'OPT',
+  OPERAND = 'OPR'
+}
+
+export default BUTTONTYPE;
---
+++ src/models/Operator.ts
@@ -0,0 +1,10 @@
+enum OPERATOR {
+  SUM = '+',
+  SUB = '-',
+  MULT = '*',
+  DIV = '/',
+  EQU = '=',
+  CLC = 'C'
+}
+
+export default OPERATOR;

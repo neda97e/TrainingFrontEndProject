55b9843be5250943224c7ad5add19dde859c3596
neda97e <76654768+neda97e@users.noreply.github.com>
2022-09-21 13:26:12 +0430

    refactor: update Button model
    update button model to make unique key without need to get it in constructor

--- src/models/ButtonSt.ts
+++ src/models/ButtonSt.ts
@@ -2,10 +2,9 @@ class ButtonSt {
   key: string;
   type: string;
   text: string;
-  // onClick: () => any;
 
-  constructor(key: string, text: string, type: string) {
-    this.key = key;
+  constructor(text: string, type: string) {
+    this.key = '_' + Math.random().toString(36).substr(2, 9);
     this.text = text;
     this.type = type;
   }
